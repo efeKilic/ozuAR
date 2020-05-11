d2d001793c56324d758363546d65252151b88561
efeKilic <effekilic@gmail.com>
2020-05-11 17:55:59 +0300

    initial commit

---
+++ Assets/scripts/menager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menager : MonoBehaviour
+{
+	public GameObject UI;
+	public void startButton()
+	{
+		UI.SetActive(false);
+	}
+}
